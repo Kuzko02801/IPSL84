4d9912fc6c61b2257cf8fd0ec4b2313de39440e6
UO271497 <58790454+UO271497@users.noreply.github.com>
2021-10-13 20:51:38 +0200

    Delete src directory

--- src/business/dataaccess/datainformation/SqliteConnectionInfo.java
+++
@@ -1,5 +0,0 @@
-package business.dataaccess.datainformation;
-
-public class SqliteConnectionInfo {
-	public static String URL = "jdbc:sqlite:C:\\Users\\Jusco\\Desktop\\IPS84\\IPS84\\src\\database\\DatosCarrera.db";
-}
