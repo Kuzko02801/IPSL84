d6771d0607f3a62b6113e419527fddb04bdcf961
carlosgarriga12 <carlosgarriga12@gmail.com>
2021-11-04 20:56:51 +0100

    base

---
+++ IPS84_copia/src/business/dataaccess/organizador/ProcesarPagos.java
@@ -0,0 +1,41 @@
+package business.dataaccess.organizador;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class ProcesarPagos {
+
+	public static void procesarArchivoDePagos(String fileName) {
+		File archivo = null;
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try {
+			archivo = new File(fileName);
+			fr = new FileReader(archivo);
+			br = new BufferedReader(fr);
+
+			String linea;
+			int nLinea = 0;
+			String idCarrera = "";
+			while ((linea = br.readLine()) != null) {
+				if (nLinea == 0) {
+					idCarrera = linea;
+				}
+				
+			}
+				
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (null != fr) {
+					fr.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+}
