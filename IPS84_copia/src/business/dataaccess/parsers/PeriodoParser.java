9bcaa3342aebfcdecc82d4f5c9e5d8a33389c321
carlosgarriga12 <carlosgarriga12@gmail.com>
2021-11-08 00:40:04 +0100

    Nueva base

---
+++ IPS84_copia/src/business/dataaccess/parsers/PeriodoParser.java
@@ -0,0 +1,28 @@
+package business.dataaccess.parsers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import business.dataaccess.dto.carrera.Periodo;
+import business.dataaccess.util.DateSqlite;
+
+public class PeriodoParser {
+	
+	public List<Periodo> devolverPeriodos (String texto) {
+		
+		List<Periodo> periodos = new ArrayList<>();
+		String [] partes = texto.split(";");
+		
+		for (String s: partes) {
+			String [] args = s.split(",");
+			String fechaInicio = args[0];
+			String fechaFin = args[1];
+			double cuota = Double.valueOf(args[2]);
+			
+			periodos.add(new Periodo(new DateSqlite(fechaInicio), new DateSqlite(fechaFin), cuota));
+		}
+		
+		return periodos;
+	}
+	
+}
