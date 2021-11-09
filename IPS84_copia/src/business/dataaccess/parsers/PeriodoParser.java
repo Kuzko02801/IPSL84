44ce042f59a377d4a0c97fd71b68f25e450cacdd
carlosgarriga12 <carlosgarriga12@gmail.com>
2021-11-09 17:24:33 +0100

    psuh

--- IPS84_copia/src/business/dataaccess/parsers/PeriodoParser.java
+++ IPS84_copia/src/business/dataaccess/parsers/PeriodoParser.java
@@ -19,6 +19,8 @@ public class PeriodoParser {
 			String fechaFin = args[1];
 			double cuota = Double.valueOf(args[2]);
 			periodos.add(new Periodo(new DateSqlite(fechaInicio), new DateSqlite(fechaFin), cuota));
+			
+			
 		}
 
 		return periodos;
