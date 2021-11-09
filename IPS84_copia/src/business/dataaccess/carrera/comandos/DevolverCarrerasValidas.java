d8ede575e47d3c6f7885ec493e7876c8d11dce80
carlosgarriga12 <carlosgarriga12@gmail.com>
2021-11-09 19:04:07 +0100

    MOSTRAR CARRERAS

--- IPS84_copia/src/business/dataaccess/carrera/comandos/DevolverCarrerasValidas.java
+++ IPS84_copia/src/business/dataaccess/carrera/comandos/DevolverCarrerasValidas.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -37,17 +38,17 @@ public class DevolverCarrerasValidas {
 		try {
 			con = DriverManager.getConnection(SqliteConnectionInfo.URL);
 			ps = con.prepareStatement(SqlStatements.SQL_CARRERAS);
-			rs = ps.executeQuery();	
-			while(rs.next()) {
-				// We check that the race is open.				
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				// We check that the race is open.
 				periodos = PeriodoParser.devolverPeriodos(rs.getString("periodos"));
-				if(Check.checkCarreraAbierta(periodos)) {
+				if (Check.checkCarreraAbierta(periodos)) {
 					carreras.add(DtoAssembler.toCarreraDto(rs));
 				}
 			}
-			
+
 			t = tableModelAssembler(carreras);
-			
+
 			ps.close();
 			rs.close();
 			con.close();
@@ -57,16 +58,29 @@ public class DevolverCarrerasValidas {
 		}
 		return t;
 	}
+
 	private TableModel tableModelAssembler(List<CarreraDto> carreras) {
-		String col[] = {"ID","Nombre","Fecha", "Tipo", "Distancia", "Plazas maximas", "Plazo1", "Cuota1", "Plazo2", "Cuota2","Plazo3","Cuota3"};
+		String col[] = { "ID", "Nombre", "Fecha", "Tipo", "Distancia", "Plazas maximas", "Plazo1", "Cuota1", "Plazo2",
+				"Cuota2", "Plazo3", "Cuota3" };
 		DefaultTableModel tableModel = new DefaultTableModel(col, 0);
 		for (CarreraDto carrera : carreras) {
-			Object[] row= {carrera.carrera_id,carrera.nombre,carrera.fecha.toString(),carrera.tipo.toString(),carrera.distancia,carrera.plazasMaximas,
-					carrera,carrera.periodos.get(0).getFechaInicio()+"-"};
+			int nPeriodos = carrera.periodos.size();
+			Object[] row = new Object[12]; 
+			List<Object> lista = new ArrayList<Object>(Arrays.asList(carrera.carrera_id, carrera.nombre, carrera.fecha.toString(), carrera.tipo.toString(),
+				carrera.distancia, carrera.plazasMaximas,
+				carrera.periodos.get(0).getFechaInicio() + " - " + carrera.periodos.get(0).getFechaFin(),
+				carrera.periodos.get(0).getCuota()));
+			for (int i = 1; i < nPeriodos; i++) {
+				lista.add(carrera.periodos.get(i).getFechaInicio() + " - " + carrera.periodos.get(i).getFechaFin());
+				lista.add(carrera.periodos.get(i).getCuota());
+			}
+			for (int i = 0; i < lista.size(); i++) {
+				row[i] = lista.get(i);
+			}
+			
 			tableModel.addRow(row);
 		}
+
 		return tableModel;
-		
 	}
-	
 }
